9191a43198c9f6db1c64846491655b621363d226
Jimmy Liu <jliu5@woolworths.com.au>
2020-03-07 21:32:15 +1100

    Add persistence layer with java JPA and derby

--- src/main/java/com/example/topic/TopicService.java
+++ src/main/java/com/example/topic/TopicService.java
@@ -1,7 +1,9 @@
 package com.example.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,33 +11,28 @@ import java.util.List;
 @Service
 public class TopicService {
 
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-        new Topic("spring1", "spring framework 1", "spring description 1"),
-        new Topic("spring2", "spring framework 2", "spring description 2"),
-        new Topic("spring3", "spring framework 3", "spring description 3")
-    ));
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> getAllTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
     public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findById(id).get();
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic) {
-        for (int i = 0; i < topics.size(); i++) {
-            if (topics.get(i).getId().equals(id)) {
-                topics.set(i, topic);
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(t -> t.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 }
